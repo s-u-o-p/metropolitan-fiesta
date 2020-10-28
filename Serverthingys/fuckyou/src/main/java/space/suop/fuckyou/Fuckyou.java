44c9c7e0442f2077ee0944926c521153d758447b
s-u-o-p <theguywhohasnoloaf@gmail.com>
2020-10-28 20:05:59 +1100

    added fuckyou
    fuck you

---
+++ Serverthingys/fuckyou/src/main/java/space/suop/fuckyou/Fuckyou.java
@@ -0,0 +1,10 @@
+package space.suop.fuckyou;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fuckyou implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
