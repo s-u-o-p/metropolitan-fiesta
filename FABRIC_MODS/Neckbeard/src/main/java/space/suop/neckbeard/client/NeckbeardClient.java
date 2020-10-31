e599b60b602a1ddf2054aee0b328c4ed8f92e9da
s-u-o-p <theguywhohasnoloaf@gmail.com>
2020-10-31 18:00:47 +1100

    Neckbeard has been added
    POGGERS!!!!!

---
+++ FABRIC_MODS/Neckbeard/src/main/java/space/suop/neckbeard/client/NeckbeardClient.java
@@ -0,0 +1,13 @@
+package space.suop.neckbeard.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class NeckbeardClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
