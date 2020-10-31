6886d1890739d3288432b72d281ab3e02dd86bf9
GenerichumanB2 <64817282+GenerichumanB2@users.noreply.github.com>
2020-10-31 19:23:38 +1100

    pushy

--- FABRIC_MODS/Neckbeard/src/main/java/space/suop/neckbeard/Neckbeard.java
+++ FABRIC_MODS/Neckbeard/src/main/java/space/suop/neckbeard/Neckbeard.java
@@ -1,10 +1,11 @@
 package space.suop.neckbeard;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.registry.RegistryKey;
 
 public class Neckbeard implements ModInitializer {
     @Override
     public void onInitialize() {
-
+    
     }
 }
